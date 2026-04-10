bd676be25e92f654a0589c82f9e33b7dc52674d5
ayat987 <ayat38699@gmail.com>
2026-04-10 03:30:07 +0200

    new problem

--- Greedy/Decrease the Sum of Digits.cpp
+++ Greedy/Decrease the Sum of Digits.cpp
@@ -8,7 +8,34 @@
     cin.tie(0), cout.tie(0);
 using namespace std;
 void ma7adesh_hayesma3ak_tool_ma_enta_da3eef(){
+     // codeforces problem
+    //  the name of prolem is the name of the file
+    //  rate :1500
+    ll n, s;
+    cin >> n >> s;
+    auto sum_d = [](ll x){
     
+        ll sum = 0;
+        while(x>0){
+            sum += x % 10;
+            x /= 10;
+        }
+        return sum;
+    };
+    if(sum_d(n)<=s){
+        cout << 0 << '\n';
+    }
+    else{
+        ll p = 1, ans = 0;
+        while(sum_d(n)>s){
+            ll c = p - (n % p); // (n%1=0) first step
+            n += c;
+            ans += c;
+            p *= 10; 
+        }
+        cout << ans << '\n';
+    }
+
 }
 int main()
 {
