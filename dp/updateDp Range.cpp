3083e9aea23556b6cdeb80d0cbbb96184eb3e897
ayat987 <ayat38699@gmail.com>
2026-04-15 23:53:18 +0200

    dp update

---
+++ dp/updateDp Range.cpp
@@ -0,0 +1,52 @@
+////رَبِّ إِنِّي لِمَا أَنْزَلْتَ إِلَيَّ مِنْ خَيْرٍ فَقِيرٌ
+#include<bits/stdc++.h>
+#define  ll long long
+#define B_ct(n) __builtin_popcountll(n)
+#define B_ (n) 31- __builtin_clz(n)
+#define SIMBA_  ios_base::sync_with_stdio(0);cin.tie(0), cout.tie(0);
+using namespace std;
+const ll N=105;
+ll dp[N][N];
+/*
+ n =2
+ 5 10
+ 100 20
+solution :((5+10)*10)+(100+10)*20)= 2350
+solution : (5+100+10)*20=2300 min
+*/
+void SIMBA() {
+   int n; cin>>n;
+    vector<ll>c(n),q(n);
+    for (int i=0;i<n;i++) {
+        cin>>c[i]>>q[i];
+    }
+    memset(dp,-1,sizeof dp);
+    function<ll(ll,ll,ll)>clc=[&](ll cur,ll lst,ll prefix) {
+        if (cur==n) {
+            if (cur!=lst) {
+                return (ll)2e18;
+            }
+            return 0ll;
+        }
+        ll &ret=dp[cur][lst];
+        if (~ret) {
+            return ret;
+        }
+        ret=2e18;
+        // Enough
+        ret=min(ret,clc(cur+1,cur+1,0)+(10+prefix+c[cur])*q[cur]);
+        //complete
+        if (cur+1<n) {
+            ret=clc(cur+1,lst,prefix+c[cur]);
+        }
+        return ret;
+    };
+    cout<<clc(0,0,0);
+}
+int main() {
+    SIMBA_
+    int t=1; //cin>>t;
+    while (t--) {
+        SIMBA();
+    }
+}
