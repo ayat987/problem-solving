933a813e011da3de95b6cb5159ddbad289be7682
ayat987 <ayat38699@gmail.com>
2026-04-29 23:03:51 +0300

    dp digit problem

--- dp/Rangeon dp.cpp
+++ dp/Rangeon dp.cpp
@@ -5,48 +5,9 @@
 #define B_ (n) 31- __builtin_clz(n)
 #define SIMBA_  ios_base::sync_with_stdio(0);cin.tie(0), cout.tie(0);
 using namespace std;
-const ll N=105;
-ll dp[N][N];
-/*
- n =2
- 5 10
- 100 20
-solution :((5+10)*10)+(100+10)*20)= 2350
-solution : (5+100+10)*20=2300 min
-*/
+
 void SIMBA() {
-   int n; cin>>n;
-    vector<ll>c(n),q(n),prefix(n+1);
-    for (int i=0;i<n;i++) {
-        cin>>c[i]>>q[i];
-    }
-    prefix[0]=0;
-    for (int i=0;i<n;i++) {
-        prefix[i+1]=prefix[i]+c[i];
-    }
-    auto add=[&](ll i,ll j) {
-        return prefix[j + 1] - prefix[i];
-    };
-    memset(dp,-1,sizeof dp);
-    function<ll(ll,ll)>clc=[&](ll cur,ll lst) {
-        if (cur==n) {
-            return 0ll;
-        }
-        ll &ret=dp[cur][lst];
-        if (~ret) {
-            return ret;
-        }
-        ret=2e18;
-        // Enough
-        ll sum=add(lst,cur);
-        ret=min(ret,clc(cur+1,cur+1)+(10+sum)*q[cur]);
-        //complete
-        if (cur+1<n) {
-            ret=clc(cur+1,lst);
-        }
-        return ret;
-    };
-    cout<<clc(0,0);
+  
 }
 int main() {
     SIMBA_
